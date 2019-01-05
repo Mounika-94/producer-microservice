df51602a7316f900b0f1e9e6c997f77bd0f48e1c
Mounika_94 <mounika0906@yahoo.com>
2019-01-05 23:53:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a sample spring micro-service application that uses Kafka API to send messages to the Kafka server.
---
+++ src/main/java/com/prft/microservices/producer/ProducerMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.prft.microservices.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerMicroServiceApplication.class, args);
+	}
+
+}
+
