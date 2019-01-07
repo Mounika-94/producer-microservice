20086943a9b0e6ff53d77d70a39c91c2a758e503
Mounika_94 <mounika0906@yahoo.com>
2019-01-07 15:33:28 +0530

    Eureka logic implementation

--- src/main/java/com/prft/microservices/producer/ProducerMicroServiceApplication.java
+++ src/main/java/com/prft/microservices/producer/ProducerMicroServiceApplication.java
@@ -2,8 +2,10 @@ package com.prft.microservices.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProducerMicroServiceApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +13,3 @@ public class ProducerMicroServiceApplication {
 	}
 
 }
-
